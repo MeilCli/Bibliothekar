1d11c8bd9d0cd3f212f0e99cb69c97e18d15d6d5
MeilCli <ics.sora.meil@gmail.com>
2022-11-30 02:01:16 +0900

    install mockk

--- Toolchain/Gradle/config/jvm/src/main/kotlin/net/meilcli/bibliothekar/toolchain/config/jvm/KotlinPlugin.kt
+++ Toolchain/Gradle/config/jvm/src/main/kotlin/net/meilcli/bibliothekar/toolchain/config/jvm/KotlinPlugin.kt
@@ -1,5 +1,15 @@
 package net.meilcli.bibliothekar.toolchain.config.jvm
 
+import net.meilcli.bibliothekar.config.Dependencies
 import net.meilcli.bibliothekar.toolchain.config.core.BasePlugin
+import org.gradle.api.artifacts.dsl.DependencyHandler
 
-class KotlinPlugin : BasePlugin()
+class KotlinPlugin : BasePlugin() {
+
+    override fun DependencyHandler.applyDependencies() {
+        superApplyDependencies()
+
+        testImplementation(Dependencies.Junit.Junit)
+        testImplementation(Dependencies.IoMockk.Mockk)
+    }
+}
