2de432b138d2ee6d268192a42730b75139417f49
MeilCli <ics.sora.meil@gmail.com>
2022-12-04 09:53:16 +0900

    add test

--- Extractor/Gradle/plugin/android/src/test/kotlin/net/meilcli/bibliothekar/extractor/plugin/jvm/BibliothekarPluginTest.kt
+++ Extractor/Gradle/plugin/android/src/test/kotlin/net/meilcli/bibliothekar/extractor/plugin/jvm/BibliothekarPluginTest.kt
@@ -1,10 +1,13 @@
 package net.meilcli.bibliothekar.extractor.plugin.jvm
 
+import org.gradle.testfixtures.ProjectBuilder
 import kotlin.test.Test
 
 class BibliothekarPluginTest {
 
     @Test
-    fun emptyTest() {
+    fun testApply() {
+        val project = ProjectBuilder.builder().build()
+        project.plugins.apply("net.meilcli.bibliothekar.extractor.plugin.android")
     }
 }
