f9043ded2576df9acb88470589f47ee7d231823b
MeilCli <ics.sora.meil@gmail.com>
2022-12-05 02:41:29 +0900

    add AndroidDynamicFeaturePlugin test

---
+++ Extractor/Gradle/plugin/android/src/functionalTest/kotlin/net/meilcli/bibliothekar/extractor/plugin/android/AndroidDynamicFeaturePluginExtractFunctionalTest.kt
@@ -0,0 +1,60 @@
+package net.meilcli.bibliothekar.extractor.plugin.android
+
+import org.gradle.testkit.runner.GradleRunner
+import org.junit.Rule
+import org.junit.Test
+import org.junit.experimental.runners.Enclosed
+import org.junit.rules.TemporaryFolder
+import org.junit.runner.RunWith
+import kotlin.test.assertNotNull
+
+@RunWith(Enclosed::class)
+class AndroidDynamicFeaturePluginExtractFunctionalTest {
+
+    class TestExtract {
+
+        @get:Rule
+        val tempFolder = TemporaryFolder()
+
+        private fun getProjectDir() = tempFolder.root
+        private fun getBuildFile() = getProjectDir().resolve("build.gradle")
+        private fun getSettingsFile() = getProjectDir().resolve("settings.gradle")
+
+        @Test
+        fun test() {
+            getSettingsFile().writeAndroidSettingText("")
+            getBuildFile().writeText(
+                """
+                    plugins {
+                        id('com.android.dynamic-feature') version '7.3.1'
+                        id('net.meilcli.bibliothekar.extractor.plugin.android')
+                    }
+                    android {
+                        compileSdkVersion 32
+                        defaultConfig {
+                            minSdkVersion 21
+                            targetSdkVersion 32
+                        }
+                        namespace 'net.meilcli.bibliothekar.test'
+                    }
+                    repositories {
+                        mavenCentral()
+                    }
+                    dependencies {
+                        implementation 'junit:junit:4.13.2'
+                    }
+                """.trimIndent()
+            )
+
+            val runner = GradleRunner.create()
+            runner.forwardOutput()
+            runner.withPluginClasspath()
+            runner.withArguments("implementationDependenciesListBibliothekarExtract", "--info")
+            runner.withDebug(true)
+            runner.withProjectDir(getProjectDir())
+            val result = runner.build()
+
+            assertNotNull("write\\s[1-9]\\soutputs\\sto".toRegex().find(result.output))
+        }
+    }
+}
