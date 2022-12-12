ea9cb69b16b4106af91ae415cf39628243118aba
MeilCli <ics.sora.meil@gmail.com>
2022-12-13 02:58:40 +0900

    add bundled module

---
+++ Extractor/Gradle/plugin/bundled/src/main/kotlin/net/meilcli/bibliothekar/extractor/plugin/bundled/BibliothekarPlugin.kt
@@ -0,0 +1,12 @@
+package net.meilcli.bibliothekar.extractor.plugin.bundled
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+@Suppress("unused")
+class BibliothekarPlugin : Plugin<Project> {
+
+    override fun apply(project: Project) {
+
+    }
+}
