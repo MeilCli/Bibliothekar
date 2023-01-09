7daa60634689c66509debf518ca91711300bf6dc
MeilCli <ics.sora.meil@gmail.com>
2023-01-09 18:16:47 +0900

    add guard when read

--- Transformer/NodeJs/core/src/maven/pom-reader.ts
+++ Transformer/NodeJs/core/src/maven/pom-reader.ts
@@ -2,6 +2,7 @@ import * as fs from "fs";
 import * as path from "path";
 import { Reader } from "../reader";
 import { Pom } from "./pom";
+import { isPom } from "./pom.guard";
 
 const pomFileName = "pom.json";
 
@@ -14,8 +15,13 @@ export class PomReader implements Reader<Pom> {
 
         const pomFiles = this.readPath(directory).filter((x) => path.basename(x) == pomFileName);
         for (const pomFile of pomFiles) {
-            console.log(pomFile);
-            result.push(JSON.parse(fs.readFileSync(pomFile).toString()) as Pom);
+            const json = JSON.parse(fs.readFileSync(pomFile).toString());
+            if (isPom(json)) {
+                result.push(json);
+            } else {
+                console.warn(`found invalid value: (${pomFile})`);
+                console.warn(json);
+            }
         }
 
         return result;
